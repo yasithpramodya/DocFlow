d44e7f0a761d4629db4c256b16b9e0946763c6eb
yasithpramodya <pramodyayasith@gmail.com>
2026-01-19 22:00:06 +0530

    Update backend URL to https://docflow-backend-ul5m.onrender.com

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://docflow-backend-ul5m.onrender.com/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
